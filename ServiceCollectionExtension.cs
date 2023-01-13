455fef5c8468c770c729fa02c37f370e440f7a9b
Ramaisha Rehman <93512967+RamaishaRehman@users.noreply.github.com>
2023-01-13 21:36:50 +0500

    Add files via upload

---
+++ ServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using LearnDigitally.Models;
+using LearnDigitally.Repository.Base;
+using LearnDigitally.Repository.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnDigitally
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            return services
+           .AddScoped<ICandidate<Candidate>, CandidateService<Candidate>>()
+           .AddScoped<IExam<Exam>, ExamService<Exam>>()
+           .AddScoped<IQuestion<Question>, QuestionService<Question>>()
+           .AddScoped<IResult<Result>, ResultService<Result>>();
+        }
+    }
+}
